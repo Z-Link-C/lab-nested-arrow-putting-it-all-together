a6d6e8605625dc239955dcd15db0ef9152bb4922
Z-Link-C <zachery.croft@gmail.com>
2026-03-09 23:56:21 -0400

    ready to grade

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 
-
+function createLoginTracker(UInfo) {
+  UInfo={ Username: UInfo.username, Password: UInfo.password }
+  let attemptCount = 0;
+  const loginAttempt = (passAttempts) => {
+    while (attemptCount < 3) {
+      attemptCount++;
+      if (passAttempts === UInfo.Password) {
+        return `Login successful`;
+      } else {
+        return `Attempt ${attemptCount}: Login failed`;
+      }
+    }
+      return `Account locked due to too many failed login attempts`;
+  }
+  return loginAttempt;
+}
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
